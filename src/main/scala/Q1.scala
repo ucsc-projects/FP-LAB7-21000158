345e5ed8773561039a1f19d7f9209a858a497e5c
Sashmitha-Ravindu <sashmitharavindu33@gmail.com>
2023-08-08 07:04:46 +0530

    Finish Q1

--- src/main/scala/Q1.scala
+++ src/main/scala/Q1.scala
@@ -1,6 +1,11 @@
 object Q1 {
+  def filterEvenNumbers(numbers: List[Int]): List[Int] = {
+    numbers.filter(number => number % 2 == 0)
+  }
   def main(args: Array[String]): Unit = {
-
+    val inputList = List(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+    val evenNumbers = filterEvenNumbers(inputList)
+    println(evenNumbers)
   }
 
 }
